9cc24ff01009ad04ecb5fc76657433e16330accb
simplykevine <umutonikevine2004@gmail.com>
2024-03-23 18:41:48 -0700

    done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Create file in /tmp
-
-file { '/tmp/school' :
-    path    => '/tmp/school',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
-    }
+# Using Puppet, create a file in /tmp
+$file_path = '/tmp/school'
+file { $file_path:
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
